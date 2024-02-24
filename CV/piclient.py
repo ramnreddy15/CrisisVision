a62d33b5d542c221e1f8774d77983b1c0e4c9375
MythicalCow <raghav.tirumale@gmail.com>
2024-02-24 14:37:33 -0600

    test image 2

--- CV/piclient.py
+++ CV/piclient.py
@@ -4,7 +4,6 @@ import time
 
 time.sleep(1)
 picam2 = Picamera2()
-picam2.create_preview_configuration({"format": "YUV420"})
 picam2.start()
-yuv420 = picam2.capture_array()
-cv2.imwrite("test.jpg", yuv420)
+test = picam2.capture_array()
+cv2.imwrite("test.jpg", test)
