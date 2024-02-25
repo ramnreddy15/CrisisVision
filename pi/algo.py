3f6ef72305cd7dfe6d2d0fd26a9e6f110155c124
MythicalCow <raghav.tirumale@gmail.com>
2024-02-24 21:10:40 -0600

    self driving algorithm integrated with live video SUCCESS

--- pi/algo.py
+++ pi/algo.py
@@ -4,6 +4,13 @@ import numpy
 import threading
 import time
 
+#Image Processing and Networking Dependencies
+import cv2
+import numpy as np
+import socket
+import struct
+from picamera2 import Picamera2
+
 #DS Sensor and Motor Drivers
 from src import ds
 from src import m
@@ -40,6 +47,8 @@ motor2 = m.Motor({
     }
 })
 
+#
+
 
 class SDNode:
     def __init__(self):
@@ -47,6 +56,13 @@ class SDNode:
         self.ds2 = 0
         self.speed = 5
         self.turnspeed = 2
+        #Start Sockets Connection to Send Real Time Image Data to Remote Monitors
+        self.clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.clientsocket.connect(('192.168.111.51',8009))
+        self.picam2 = Picamera2()
+        self.config = self.picam2.create_preview_configuration({'format': 'RGB888'})
+        self.picam2.configure(self.config)
+        self.picam2.start()
 
     def getDSData(self):
         while True:
@@ -63,16 +79,31 @@ class SDNode:
             elif(self.ds1 > 0.5 and self.ds2 < 0.5):
                 motor2.backward(self.turnspeed)
                 motor1.forward(self.turnspeed)
+                time.sleep(0.1)
+                motor1.stop()
+                motor2.stop()
             elif(self.ds1 < 0.5 and self.ds2 > 0.5):
                 motor1.backward(self.turnspeed)
                 motor2.forward(self.turnspeed)
+                time.sleep(0.1)
+                motor1.stop()
+                motor2.stop()
             else:
                 motor1.forward(self.speed)
                 motor2.forward(self.speed)
 
+    def imgProc(self):
+        while True:
+            frame = self.picam2.capture_array()
+            frameS = frame.tobytes()
+            message_size = struct.pack(">Q", len(frameS))
+            self.clientsocket.sendall(message_size)
+            self.clientsocket.sendall(frameS)
+
     def spawnThreads(self):
         threading.Thread(target=self.getDSData).start()
         threading.Thread(target=self.selfDrive).start()
+        threading.Thread(target=self.imgProc).start()
 
 
 def main():
