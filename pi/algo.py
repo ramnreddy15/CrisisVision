881269e57b945d9d33fe69dc6fecd77f2dd1bd66
MythicalCow <raghav.tirumale@gmail.com>
2024-02-25 01:51:23 -0600

    algorithm complete

--- pi/algo.py
+++ pi/algo.py
@@ -49,10 +49,12 @@ motor2 = m.Motor({
 
 class SDNode:
     def __init__(self):
+        #Vehicle Clocking Variable
+        self.clock = False
         #Vehicles Control Variables and Data
         self.ds1 = 0
         self.ds2 = 0
-        self.speed = 5
+        self.speed = 2
         self.turnspeed = 1
         self.numPano = 5
         self.panaImages = [0 for i in range(self.numPano)]
@@ -68,91 +70,107 @@ class SDNode:
         self.picam2.start()
 
 
-
     def getDSData(self):
         while True:
-            self.ds1 = distance_sensor1.distance
-            self.ds2 = distance_sensor2.distance
-            time.sleep(0.1)
-
+            if self.clock:
+                self.ds1 = distance_sensor1.distance
+                self.ds2 = distance_sensor2.distance
+                time.sleep(0.1)
+            else:
+                break
     def selfDrive(self):
         global motor1,motor2
         while True:
-            if(self.ds1 < 0.5 and self.ds2 < 0.5):
-                motor1.backward(self.speed)
-                motor2.backward(self.speed)
-            elif(self.ds1 > 0.5 and self.ds2 < 0.5):
-                motor2.backward(self.turnspeed)
-                motor1.forward(self.turnspeed)
-                time.sleep(0.1)
-                motor1.stop()
-                motor2.stop()
-                time.sleep(0.1)
-            elif(self.ds1 < 0.5 and self.ds2 > 0.5):
-                motor1.backward(self.turnspeed)
-                motor2.forward(self.turnspeed)
-                time.sleep(0.1)
-                motor1.stop()
-                motor2.stop()
-                time.sleep(0.1)
-            else:
-                motor1.forward(self.speed)
-                motor2.forward(self.speed)
-
+            if self.clock:
+                if(self.ds1 < 0.5 and self.ds2 < 0.5):
+                    motor1.backward(self.speed)
+                    motor2.backward(self.speed)
+                    time.sleep(0.2)
+                elif(self.ds1 > 0.5 and self.ds2 < 0.5):
+                    motor2.backward(self.turnspeed)
+                    motor1.forward(self.turnspeed)
+                    time.sleep(0.2)
+                    motor1.stop()
+                    motor2.stop()
+                    time.sleep(0.1)
+                elif(self.ds1 < 0.5 and self.ds2 > 0.5):
+                    motor1.backward(self.turnspeed)
+                    motor2.forward(self.turnspeed)
+                    time.sleep(0.2)
+                    motor1.stop()
+                    motor2.stop()
+                    time.sleep(0.1)
+                else:
+                    motor1.forward(self.speed)
+                    motor2.forward(self.speed)
+                    time.sleep(0.2)
+            else: 
+                break
     def imgWrite(self):
-        while True:        
+        while True:
             self.frame = self.picam2.capture_array()
 
 
     def imgRelay(self):
         while True:
-            print(self.frame.size, self.frame.dtype)
-            frameS = self.frame.tobytes()
-            message_size = struct.pack(">Q", len(frameS))
-            self.clientsocket.sendall(message_size)
-            self.clientsocket.sendall(frameS)
-
+            if self.clock:    
+                print(self.frame.size, self.frame.dtype)
+                frameS = self.frame.tobytes()
+                message_size = struct.pack(">Q", len(frameS))
+                self.clientsocket.sendall(message_size)
+                self.clientsocket.sendall(frameS)
+            else: 
+                break
     def panoramicDrive(self):
         while True:
-            motor1.backward(self.turnspeed)
-            motor2.forward(self.turnspeed)
-            time.sleep(0.1)
-            motor1.stop()
-            motor2.stop()
-            time.sleep(1)
-
+            if not self.clock:    
+                motor1.backward(self.turnspeed)
+                motor2.forward(self.turnspeed)
+                time.sleep(0.1)
+                motor1.stop()
+                motor2.stop()
+                time.sleep(1)
+            else: 
+                break
     def panoramicMeshSend(self):
         while True:
-            if(self.panacount < self.numPano):
-                self.panaImages[self.panacount] = self.frame
-                self.panacount += 1
-                time.sleep(0.4)
-            else:
-                stitcher = cv2.Stitcher_create()
-                status, result = stitcher.stitch(self.panaImages)
-                resultS = np.array(result)
-                print(resultS.shape)
-                size = str(resultS.shape).encode()
-                resultS = resultS.tobytes()
-                lR = len(resultS)
-                print(lR)
-                if(lR > 11):
-                    message_size = struct.pack(">Q", len(size))
-                    self.clientsocket.sendall(message_size)
-                    self.clientsocket.sendall(size)
-                    message_size = struct.pack(">Q", len(resultS))
-                    self.clientsocket.sendall(message_size)
-                    self.clientsocket.sendall(resultS)
-                self.panacount = 0
-               
+            if not self.clock:
+                if(self.panacount < self.numPano):
+                    self.panaImages[self.panacount] = self.frame
+                    self.panacount += 1
+                    time.sleep(0.4)
+                else:
+                    stitcher = cv2.Stitcher_create()
+                    status, result = stitcher.stitch(self.panaImages)
+                    resultS = np.array(result)
+                    print(resultS.shape)
+                    size = str(resultS.shape).encode()
+                    resultS = resultS.tobytes()
+                    lR = len(resultS)
+                    print(lR)
+                    if(lR > 11):
+                        message_size = struct.pack(">Q", len(size))
+                        self.clientsocket.sendall(message_size)
+                        self.clientsocket.sendall(size)
+                        message_size = struct.pack(">Q", len(resultS))
+                        self.clientsocket.sendall(message_size)
+                        self.clientsocket.sendall(resultS)
+                    self.panacount = 0
+            else: 
+                break
+
     def spawnThreads(self):
-        threading.Thread(target=self.getDSData).start()
-        threading.Thread(target=self.selfDrive).start()
-        threading.Thread(target=self.imgRelay).start()
         threading.Thread(target=self.imgWrite).start()
-        #threading.Thread(target=self.panoramicDrive).start()
-        #threading.Thread(target=self.panoramicMeshSend).start()
-        
+        while True:
+            self.clock = not self.clock
+            threading.Thread(target=self.getDSData).start()
+            threading.Thread(target=self.selfDrive).start()
+            threading.Thread(target=self.imgRelay).start()
+            time.sleep(45)
+            self.clock = not self.clock
+            threading.Thread(target=self.panoramicDrive).start()
+            threading.Thread(target=self.panoramicMeshSend).start()
+            time.sleep(15) 
 
 
 def main():
