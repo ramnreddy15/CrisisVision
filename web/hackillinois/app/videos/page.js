3cb01e784cac5d0c34bcf82d49122ceb1b31533e
Lex Ibanez <laibanez@uci.edu>
2024-02-25 06:46:43 -0600

    added youtube embed links

--- web/hackillinois/app/videos/page.js
+++ web/hackillinois/app/videos/page.js
@@ -12,12 +12,11 @@ export default function Page() {
         </div>
         <div className="relative h-full w-[85%] left-[7.5%] top-[200px] flex flex-col justfiy-center gap-[110px]">
           <div className="w-full h-auto flex flex-wrap justify-space-between justify-center items-center gap-[110px] ">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/QEDnYhGgfKs?si=TwfC4a-m_svb5Us1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/kV3XZdZel-U?si=zQSNzdy6V-rE0Uhv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           </div>
           <div className="w-full h-auto flex flex-wrap justify-space-between justify-center items-center gap-[110px] ">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            
           </div>
         </div>
         
