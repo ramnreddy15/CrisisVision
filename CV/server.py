08a10c23325022f7eb35a3e36158a25d6dfc8e8c
Ram Reddy <ram.n.reddy15@gmail.com>
2024-02-24 20:16:24 -0600

    Working with PI!!!!

--- CV/server.py
+++ CV/server.py
@@ -1,10 +1,11 @@
-import pickle
+import json
 import socket
-import struct
+from struct import unpack
+import numpy as np
 
 import cv2
 
-HOST = '192.168.252.241'
+HOST = '192.168.111.51'
 PORT = 8009
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,29 +19,43 @@ print('Socket now listening')
 conn, addr = s.accept()
 print("Client connected at ", addr)
 
-data = b'' ### CHANGED
-payload_size = struct.calcsize("L") ### CHANGED
+while(True):
+    bs = conn.recv(8)
+    (length,) = unpack('>Q', bs)
+    data = b''
+    while len(data) < length:
+        # doing it in batches is generally better than trying
+        # to do it all in one go, so I believe.
+        to_read = length - len(data)
+        data += conn.recv(
+            4096 if to_read > 4096 else to_read)
 
-while True:
-
-    # Retrieve message size
-    while len(data) < payload_size:
-        data += conn.recv(4096)
-
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("L", packed_msg_size)[0] ### CHANGED
-
-    # Retrieve all data based on message size
-    while len(data) < msg_size:
-        data += conn.recv(4096)
-
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-
-    # Extract frame
-    frame = pickle.loads(frame_data)
-
-    # Display
-    cv2.imshow('frame', frame)
+    # send our 0 ack
+    data = json.loads(data.decode())
+    data = np.array(data)
+    # cv2.imwrite("test.jpg", data)
+    cv2.imshow('frame', data)
     cv2.waitKey(1)
+
+# sockfile = conn.makefile('rb')
+
+# payload_size = struct.calcsize("L") ### CHANGED
+
+# while True:
+
+#     # Retrieve message size
+#     packed_msg_size = sockfile.read(payload_size)
+
+#     msg_size = struct.unpack("L", packed_msg_size)[0] ### CHANGED
+
+#     # Retrieve all data based on message size
+
+#     frame_data = sockfile.read(msg_size)
+
+
+#     # Extract frame
+#     frame = json.loads(frame_data)
+
+#     # Display
+#     cv2.imshow('frame', frame)
+#     cv2.waitKey(1)
