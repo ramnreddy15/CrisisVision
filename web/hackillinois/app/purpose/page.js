2a4300475fa12ceb5106c3aa69cf2daa1b256de2
Lex Ibanez <laibanez@uci.edu>
2024-02-25 04:39:56 -0600

    purpose page updates

--- web/hackillinois/app/purpose/page.js
+++ web/hackillinois/app/purpose/page.js
@@ -18,19 +18,24 @@ export default function Page() {
             <div>
             <div className="absolute bg-[#403D39] rounded-md h-[250px] w-[90%] left-[7.5%] top-[250px] text-xl text-white flex flex-wrap text-pretty justify-center items-center">
                 <div className="p-10">
-                    <p className="text-center">From 2018 to 2022, they have caused over $600 billion in damages and 1,751 deaths. In 2022, 96 Firefighters were killed while on duty.
-                    Search and rescue is crucial during situations such as natural disasters to save lives. An important part of an effective search and rescue 
-                    operation is the ability quickly and effectively assess a situation and decide how to proceed. However, these natural disasters  can present 
-                    dangers to the search and rescue teams, as the harsh weather conditions and unstable terrain can pose a risk to their safety. We aimed to create 
-                    a device that can autonomously collect data that the search and rescue team can use the assess the situation without risking their safety.</p>
+                    <p className="text-center">
+                        In the span from 2018 to 2022, natural disasters have inflicted over $600 billion in damages and claimed the lives of 1,751 individuals.
+                        Tragically, in 2022 alone, 96 firefighters lost their lives while bravely serving on duty. These statistics underscore the critical importance of 
+                        search and rescue (SAR) operations during such calamities, where every second can mean the difference between life and death.
+
+                        The ability to swiftly and accurately assess a crisis situation is pivotal for effective SAR operations. However, the very nature of 
+                        natural disasters exposes SAR teams to perilous conditions, including harsh weather and unstable terrain, heightening the risk to their safety.</p>
                     </div>
                 </div>
-                <div className="absolute bg-[#403D39] rounded-md h-[250px] w-[90%] left-[7.5%] top-[550px] p-10 text-xl text-white flex flex-wrap text-pretty justify-center items-center">
-                <p className="text-center">People often can get stranded and trapped as a result of the rubble and destruction caused by
-                  them. Because of this, Search and Rescue is crucial to save lives. However, these dangerous conditions can endanger the SAR responders.
-                 Being able to quickly assess the situation is important for these operations, but ensuring responder's safety is equally as important. That's why we created an 
-                 autonomous vehicle that can help SAR teams survey an area. CrisisVision can autonomously navigate dangerous environments and give a live feed of what it sees. 
-                 It can also recognize objects, hazards, or people in distress.</p>
+                <div className="absolute bg-[#403D39] rounded-md h-[325px] w-[90%] left-[7.5%] top-[550px] p-10 text-xl text-white flex flex-wrap text-pretty justify-center items-center">
+                <p className="text-center">In response to these challenges, we have developed an innovative solution: CrisisVision, an autonomous vehicle designed to gather essential data without
+                 jeopardizing the safety of SAR teams. In disaster zones where individuals may become stranded or trapped amid rubble and destruction, SAR intervention becomes indispensable.
+
+                CrisisVision serves as an indispensable ally to SAR teams, autonomously navigating treacherous environments while providing real-time visual feedback. Its advanced technology 
+                enables it to identify objects, hazards, and individuals in distress, empowering SAR personnel with critical information for informed decision-making.
+
+                By deploying CrisisVision, SAR operations become more efficient and effective, minimizing the need for direct human exposure to danger. Our mission is clear: to safeguard the 
+                lives of both those in distress and the courageous responders who selflessly serve on the frontlines of disaster relief.</p>
                 </div>
             </div>
         </div>
