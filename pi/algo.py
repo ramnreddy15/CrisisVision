f05dcef0265a046a38de28136668f8147c8bec6b
MythicalCow <raghav.tirumale@gmail.com>
2024-02-25 00:56:21 -0600

    panoramic mode finished

--- pi/algo.py
+++ pi/algo.py
@@ -53,8 +53,12 @@ class SDNode:
         self.ds1 = 0
         self.ds2 = 0
         self.speed = 5
-        self.turnspeed = 2
+        self.turnspeed = 1
+        self.numPano = 5
+        self.panaImages = [0 for i in range(self.numPano)]
+        self.panacount = 0
         self.frame = np.zeros((480,640,3), dtype='uint8')
+        
         #Start Sockets Connection to Send Real Time Image Data to Remote Monitors
         self.clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.clientsocket.connect(('192.168.111.51',8009))
@@ -62,6 +66,8 @@ class SDNode:
         self.config = self.picam2.create_preview_configuration({'format': 'RGB888'})
         self.picam2.configure(self.config)
         self.picam2.start()
+
+
 
     def getDSData(self):
         while True:
@@ -106,13 +112,47 @@ class SDNode:
             self.clientsocket.sendall(message_size)
             self.clientsocket.sendall(frameS)
 
-           
+    def panoramicDrive(self):
+        while True:
+            motor1.backward(self.turnspeed)
+            motor2.forward(self.turnspeed)
+            time.sleep(0.1)
+            motor1.stop()
+            motor2.stop()
+            time.sleep(1)
+
+    def panoramicMeshSend(self):
+        while True:
+            if(self.panacount < self.numPano):
+                self.panaImages[self.panacount] = self.frame
+                self.panacount += 1
+                time.sleep(0.4)
+            else:
+                stitcher = cv2.Stitcher_create()
+                status, result = stitcher.stitch(self.panaImages)
+                resultS = np.array(result)
+                print(resultS.shape)
+                size = str(resultS.shape).encode()
+                resultS = resultS.tobytes()
+                lR = len(resultS)
+                print(lR)
+                if(lR > 11):
+                    message_size = struct.pack(">Q", len(size))
+                    self.clientsocket.sendall(message_size)
+                    self.clientsocket.sendall(size)
+                    message_size = struct.pack(">Q", len(resultS))
+                    self.clientsocket.sendall(message_size)
+                    self.clientsocket.sendall(resultS)
+                self.panacount = 0
+               
     def spawnThreads(self):
         threading.Thread(target=self.getDSData).start()
         threading.Thread(target=self.selfDrive).start()
         threading.Thread(target=self.imgRelay).start()
         threading.Thread(target=self.imgWrite).start()
-
+        #threading.Thread(target=self.panoramicDrive).start()
+        #threading.Thread(target=self.panoramicMeshSend).start()
+        
 
 
 def main():
