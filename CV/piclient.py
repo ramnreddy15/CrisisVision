f575255620a048f55e1a210949f719d7b4138401
MythicalCow <raghav.tirumale@gmail.com>
2024-02-24 20:17:56 -0600

    basic image send from raspberry pi to host

--- CV/piclient.py
+++ CV/piclient.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import socket
 import sys
-import pickle
+import json
 import struct
 from picamera2 import Picamera2
 import time
@@ -11,20 +11,16 @@ import time
 # cap.set(cv2.CAP_PROP_FRAME_WIDTH,)
 # cap.set(cv2.CAP_PROP_FRAME_WIDTH,480)
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('192.168.111.241',8009))
+clientsocket.connect(('192.168.111.51',8009))
 # clientsocket.connect(('localhost',8089))
 picam2 = Picamera2()
 config = picam2.create_preview_configuration({'format': 'RGB888'})
 picam2.configure(config)
 picam2.start()
-
-while True:
-    frame = picam2.capture_array()
-    #framef = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
-    data = pickle.dumps("hi from pi")
-
-    # Send message length first
-    message_size = struct.pack("L", len(data)) ### CHANGED
-
-    # Then data
-    clientsocket.sendall(message_size + data)
+frame = picam2.capture_array()
+frameS = json.dumps(frame.tolist()).encode()
+#framef = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+#data = pickle.dumps(frame)
+message_size = struct.pack(">Q", len(frameS)) ### CHANGED
+clientsocket.sendall(message_size)
+clientsocket.sendall(frameS)
